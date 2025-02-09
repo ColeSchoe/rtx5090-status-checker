0ae8d10d6cd9f0b06f2e070fe89256a0d91bbff2
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2025-02-08 21:54:09 -0600

    Created send_email.py with the purpose of sending out emails an in-stock item is found

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ 
+def send_message(message):
+    # Start server, set connection to tls mode, log in, and send message
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(os.getenv("EMAIL"), os.getenv("PW"))
+    server.sendmail(os.getenv("EMAIL"), os.getenv("EMAIL"), message)
+    server.quit()
+ 
+if __name__ == "__main__":
+    send_message("Hi from me!")
