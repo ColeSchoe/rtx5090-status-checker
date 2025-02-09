36ed478cf6853e1df8ce9d52fa211ffdd9fedf5d
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2025-02-08 18:05:28 -0600

    Created newegg_scraper.py that performs a similar function to micro_center_scraper.py

---
+++ newegg_scraper.py
@@ -0,0 +1,81 @@
+import csv, os, time, datetime
+from random import randint
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from RTX5090Listing import RTX5090Listing
+
+rtx_5090_page_newegg = "https://www.newegg.com/p/pl?N=100007709%20601469153"
+
+def searchThroughNewegg(url):
+    page = webdriver.Chrome()
+
+    while(True):
+        page.get(url)
+        time.sleep(randint(3,10))
+        unfiltered_listings = page.find_elements(By.CLASS_NAME, "item-cell")
+        number_of_listings = unfiltered_listings.__len__()
+        current_listing = 0
+        time.sleep(randint(2,5))
+        while current_listing < number_of_listings:
+            current_5090 = RTX5090Listing()
+
+            # Finding the product name
+            product_name = page.find_elements(By.CLASS_NAME, "item-title")[current_listing].text
+            current_5090.product_name = product_name
+
+            # Finding the product price
+            try:
+                price_element = page.find_elements(By.CSS_SELECTOR, "li.price-current")[current_listing].text
+                current_5090.price = price_element
+            except (Exception):
+                break
+
+            # Finding the stock and availability of the product
+            try:
+                stock_element = page.find_elements(By.CSS_SELECTOR, "p.item-promo")[current_listing].text
+                if "OUT OF STOCK" in stock_element:
+                    current_5090.no_units = 0
+                    current_5090.available = False
+                else:
+                    current_5090.no_units = "In-store pickup only"
+                    current_5090.available = True
+                
+            except (Exception):
+                stock_element = page.find_elements(By.CSS_SELECTOR, "p.item-promo")[-1].text
+                if "OUT OF STOCK" in stock_element:
+                    current_5090.no_units = 0
+                    current_5090.available = False
+                else:
+                    current_5090.no_units = "?"
+                    current_5090.available = True
+            
+            # Link to product page
+            link_element = page.find_elements(By.CLASS_NAME, "item-info")[current_listing].get_attribute("href")
+            current_5090.link = link_element
+
+            # Location and time found information
+            current_5090.store = "Newegg"
+            current_5090.time = datetime.datetime.now()
+
+            with open(csv_file, "a", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([current_5090.product_name, current_5090.price, current_5090.available, current_5090.no_units, current_5090.store, current_5090.time, current_5090.link])
+
+            current_listing += 1
+        break
+
+    return
+
+
+if __name__ == "__main__":
+    csv_file = "neweggdata.csv"
+
+    # Opening a file to put quotes in csv form, creating it if it doesn't already exist
+    if not os.path.exists(csv_file):
+        with open(csv_file, "w", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Product Name", "Price", "Available", "No. Units", "Store", "Time", "Link"])
+    
+    # Search through Newegg's site for information on 5090 listings
+    searchThroughNewegg(rtx_5090_page_newegg)
