374f47f9465e307cac9980a9f1499c79d1b23e42
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2025-02-09 13:34:50 -0600

    changed send_message() to send a text message to verizon recipients that uses an env var for the phone number. So automated messages are now texts instead of emails

--- send_email.py
+++ send_email.py
@@ -3,14 +3,27 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def send_email(message):
+    # Start server, set connection to tls mode, log in, and send message
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(os.getenv("EMAIL"), os.getenv("APP_PW"))
+    
+    server.sendmail(os.getenv("EMAIL"), os.getenv("EMAIL"), message)
+
+    server.quit()
  
 def send_message(message):
     # Start server, set connection to tls mode, log in, and send message
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(os.getenv("EMAIL"), os.getenv("APP_PW"))
-    server.sendmail(os.getenv("EMAIL"), os.getenv("EMAIL"), message)
+
+    recipient = os.getenv("PHONE_NUM") + "@vtext.com"
+    server.sendmail(os.getenv("EMAIL"), recipient, message)
+
     server.quit()
  
 if __name__ == "__main__":
-    send_message("Hi from me again, this should be important!")
+    send_message("Hi from me again, this should be a text!")
